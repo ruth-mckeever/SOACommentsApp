a0686c055e01f3281f76dd75091df9e534780477
Ruth McKeever <ruth.mckeever@dkit.ie>
2024-10-03 15:27:51 +0100

    Add project files.

---
+++ Services/SanitizeService.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using System.Xml.Serialization; 
+
+
+namespace SOACommentsApp.Services
+{
+    public class SanitizeService
+    {
+        static readonly string Sanitize_URL = "http://www.purgomalum.com/service/xml";
+
+        public string SanitizeText(string rawText)
+        {
+            var client = new RestClient(Sanitize_URL);
+            var request = new RestRequest();
+            request.AddParameter("text", rawText);
+            var response = client.Execute(request);
+            if(!string.IsNullOrWhiteSpace(response.Content))
+            {
+
+
+                var serializer = new XmlSerializer(typeof(PurgoMalum));
+                using (StringReader sr = new(response.Content))
+                {
+                    PurgoMalum? filtered = (PurgoMalum?)serializer.Deserialize(sr);
+                    return filtered.result;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ UserComment.cs
@@ -0,0 +1,45 @@
+﻿using SOACommentsApp.Services;
+
+namespace SOACommentsApp
+{
+    public class UserComment
+    {
+        public string RawComment { get; set; }
+        public string SanitizedComment { get; set; }
+
+        public SanitizeService sanitizer = new SanitizeService();
+
+        public UserComment(string rawComment)
+        {
+            RawComment = rawComment;
+            SanitizedComment = sanitizer.SanitizeText(rawComment);
+        }
+    }
+
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.purgomalum.com")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.purgomalum.com", IsNullable = false)]
+    public partial class PurgoMalum
+    {
+
+        private string resultField;
+
+        /// <remarks/>
+        public string result
+        {
+            get
+            {
+                return this.resultField;
+            }
+            set
+            {
+                this.resultField = value;
+            }
+        }
+    }
+
+}
